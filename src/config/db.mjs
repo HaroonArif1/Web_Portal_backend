e97a684adec89c2ddf86d26fd4e95d940529d09a
Rehan Aslam <rabscs@gmail.com>
2026-02-02 02:06:17 +0500

    fix: db connection string updated ...

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -3,7 +3,9 @@ import { env } from './env.mjs';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(env.MONGO_URI);
+    await mongoose.connect(env.MONGO_URI, {
+      dbName: 'customer_portal'
+    });
     console.log('MongoDB connected');
     
   } catch (error) {
