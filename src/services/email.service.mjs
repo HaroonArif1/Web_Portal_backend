cc2a05b3079545c281dc2fd173d1a052b09c7dfb
Rehan Aslam <rabscs@gmail.com>
2026-02-04 02:00:53 +0500

    fix: quick fix in email ...

--- src/services/email.service.mjs
+++ src/services/email.service.mjs
@@ -2,10 +2,12 @@ import sgMail from '@sendgrid/mail';
 import { env } from '../config/env.mjs';
 
 
-// sgMail.setDataResidency('eu');
+sgMail.setDataResidency('eu');
 sgMail.setApiKey(env.SENDGRID_API_KEY);
 
 export const sendOTPEmail = async (email, otp) => {
+  console.log({from: env.SENDGRID_FROM_EMAIL});
+  
   try{
     await sgMail.send({
       to: email,
