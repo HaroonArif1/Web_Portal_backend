9df863d3a51045e4edb37c322889fc6522274840
Rehan Aslam <rabscs@gmail.com>
2026-02-08 17:52:19 +0500

    fix: quick fix ...

--- src/controllers/user.controller.mjs
+++ src/controllers/user.controller.mjs
@@ -3,10 +3,13 @@ import { Balance, Products } from '../models/index.mjs';
 export const dashboard = async (req, res) => {
   const { account_id: AccountId } = req.user;
 
-  const balances = await Balance.find({ AccountId, ProductId: 1 }).lean().exec();
-  const totalBalance = balances.reduce((acc, curr) => acc + +curr.Amount, 0) || 0;
-  
-  const productIds = balances.map(b => b.ProductId);
+  const balances = await Balance.find({ AccountId, ProductId: {$in: [1, 35]} }).lean().exec();
+  const bal = await balances.map(item => {
+    if(item.ProductId === 35) return ({ ...item, Amount: item.Amount * 0.112})
+      else return item;
+  })
+  const totalBalance = bal.reduce((acc, curr) => acc + +curr.Amount, 0) || 0;
+  const productIds = bal.map(b => b.ProductId);
   
   // Fetch products related to the balances
   const products = await Products.find({ ProductId: { $in: productIds } }).lean().exec();
